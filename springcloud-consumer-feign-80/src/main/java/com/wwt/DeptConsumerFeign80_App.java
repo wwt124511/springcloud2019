29a39c5801a89402f699cea7807c16d637369c2e
阁楼望月 <1369967473@qq.com>
2019-03-17 19:01:16 +0800

    ribbon、feign客户端代码提交
    主要是ribbon和feign客户端的启动类都加上了@EnableDiscoveryClient注解，不加上就找不到eureka上注册的服务

--- springcloud-consumer-feign-80/src/main/java/com/wwt/DeptConsumerFeign80_App.java
+++ springcloud-consumer-feign-80/src/main/java/com/wwt/DeptConsumerFeign80_App.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DeptConsumerFeign80_App
 {
