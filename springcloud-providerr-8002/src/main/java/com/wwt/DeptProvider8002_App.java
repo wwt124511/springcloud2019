9d0aeff9b1d24d77cd12fc4d5b8fd84b32dfe55e
阁楼望月 <1369967473@qq.com>
2019-03-18 16:53:52 +0800

    provider增加8002节点

---
+++ springcloud-providerr-8002/src/main/java/com/wwt/DeptProvider8002_App.java
@@ -0,0 +1,18 @@
+package com.wwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class DeptProvider8002_App {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DeptProvider8002_App.class, args);
+    }
+}
--- springcloud-providerr-8002/src/main/java/com/wwt/controller/DeptController.java
+++ springcloud-providerr-8002/src/main/java/com/wwt/controller/DeptController.java
@@ -8,20 +8,20 @@ public class DeptController {
 
     @RequestMapping(value = "/dept/add")
     public String add() {
-        System.out.println("8001-/dept/add");
+        System.out.println("8002-/dept/add");
         return "添加成功";
     }
 
 
     @RequestMapping(value = "/dept/get")
     public String get(){
-        System.out.println("8001-/dept/get");
+        System.out.println("8002-/dept/get");
         return "deptname";
     }
 
     @RequestMapping(value = "/dept/list")
     public String list(){
-        System.out.println("8001-/dept/list");
+        System.out.println("8002-/dept/list");
         return "dept list";
     }
 
